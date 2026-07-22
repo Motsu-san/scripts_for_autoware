dfd42af19b8cd02d4a90847d4eef4433ccebb7d8
Motsu-san <83898149+Motsu-san@users.noreply.github.com>
2026-07-22 21:16:43 +0900

    feat: enhance build_commands.sh for improved repository management and build processes
    - Updated repository import paths to a new directory structure for better organization.
    - Added commands for system updates and rosdep updates to ensure dependencies are current.
    - Introduced multiple build commands for specific packages, enhancing flexibility in building various components.
    - Improved logging for build processes to facilitate easier debugging and tracking of build outputs.
    
    These changes streamline the build process and improve the overall usability of the build_commands.sh script for developers.
    
    Signed-off-by: Motsu-san <83898149+Motsu-san@users.noreply.github.com>

--- sh/build_commands.sh
+++ sh/build_commands.sh
@@ -14,10 +14,15 @@ mkdir src
 
 vcs import src < autoware.repos
 vcs import src < tools.repos
-vcs import src < autoware-nightly.repos
+vcs import src < repositories/autoware.repos
+vcs import src < repositories/tools.repos
+vcs import src < repositories/simulator.repos
+vcs import src < repositories/autoware-nightly.repos
 
 source /opt/ros/humble/setup.bash
+sudo apt update && sudo apt upgrade -y
 rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
+rosdep update
 
 # build (no limit on CPU usage)
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
@@ -26,17 +31,55 @@ colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --continu
 
 # build (limiting CPU usage)
 MAKEFLAGS="-j16" colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error 2>&1 | tee build.log
+# build for autoware
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --packages-up-to \
+  tier4_localization_launch autoware_default_adapi_universe autoware_adapi_adaptors sample_sensor_kit_description sample_sensor_kit_launch ndt_direct_measure \
+  2>&1 | tee build.log
+# build (limiting CPU usage) for pilot-auto.x2
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --cmake-clean-cache --packages-up-to \
+  tier4_localization_launch autoware_default_adapi_universe autoware_adapi_adaptors individual_params j6_gen2_description \
+  tier4_external_api_msgs aip_x2_gen2_description aip_x2_gen2_launch \
+  2>&1 | tee build.log
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --packages-up-to \
+  tier4_localization_launch autoware_default_adapi_universe autoware_adapi_adaptors individual_params j6_gen2_description \
+  tier4_external_api_msgs imu_monitor autoware_cuda_pointcloud_preprocessor autoware_localization_rviz_plugin autoware_lanelet2_map_visualizer \
+  2>&1 | tee build.log
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --cmake-clean-cache --packages-up-to \
+  tier4_localization_launch default_ad_api ad_api_adaptors individual_params j6_gen1_description \
+  2>&1 | tee build.log
+# build (limiting CPU usage) for pilot-auto.xx1
+MAKEFLAGS="-j16" colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --packages-up-to \
+  tier4_localization_launch autoware_default_adapi ad_api_adaptors individual_params jpntaxi_description \
+  2>&1 | tee build.log
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --packages-up-to \
+  tier4_localization_launch autoware_stop_filter autoware_ekf_localizer autoware_twist2accel autoware_pose_instability_detector autoware_localization_error_monitor \
+  tier4_sensing_launch pe_ars408_ros aip_xx1_launch individual_params autoware_launch \
+  tier4_localization_rviz_plugin \
+  tier4_system_launch tier4_vehicle_launch diagnostic_graph_aggregator \
+  jpntaxi_launch jpntaxi_description aip_xx1_description \
+  2>&1 | tee build.log
+MAKEFLAGS="-j16" colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --packages-up-to \
+  tier4_localization_launch aip_xx1_launch individual_params autoware_launch \
+  autoware_localization_rviz_plugin \
+  jpntaxi_description aip_xx1_description \
+  2>&1 | tee build.log
 
 # build again (limiting CPU usage)
 MAKEFLAGS="-j16" colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error 2>&1 | tee -a build.log
+# build replay localization packages
+MAKEFLAGS="-j16" colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --packages-up-to $(cat localization_packages.txt | grep -v '^#' | tr '\n' ' ') 2>&1 | tee build.log
+MAKEFLAGS="-j16" colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --cmake-clean-cache --packages-up-to $(cat localization_packages.txt | grep -v '^#' | tr '\n' ' ') 2>&1 | tee build.log
+MAKEFLAGS="-j16" colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --packages-select $(cat localization_packages.txt | grep -v '^#' | tr '\n' ' ') 2>&1 | tee build.log
+MAKEFLAGS="-j16" colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --cmake-clean-cache --packages-select $(cat localization_packages.txt | grep -v '^#' | tr '\n' ' ') 2>&1 | tee build.log
 # build again (no limit on CPU usage)
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --continue-on-error 2>&1 | tee -a build.log
 
 # Rebuild
 vcs import src < autoware.repos
-vcs import src < tools.repos
-vcs import src < simulator.repos
-vcs import src < autoware-nightly.repos
+vcs import src < repositories/autoware.repos
+vcs import src < repositories/tools.repos
+vcs import src < repositories/simulator.repos
+vcs import src < repositories/autoware-nightly.repos
 vcs pull src
 
 # 環境変数をクリーンアップ
@@ -55,11 +98,39 @@ rosdep update
 
 # re-build limiting CPU usage
 MAKEFLAGS="-j16" colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --cmake-clean-cache 2>&1 | tee build.log
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error 2>&1 | tee build.log
+# MAKEFLAGS="-j16" colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --parallel-workers 8 --continue-on-error --cmake-clean-cache 2>&1 | tee build.log
+
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --parallel-workers 8 --continue-on-error --packages-up-to tier4_localization_launch aip_xx1_launch individual_params tier4_localization_rviz_plugin 2>&1 | tee build.log
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --parallel-workers 8 --continue-on-error --packages-up-to tier4_localization_launch aip_xx1_launch individual_params autoware_localization_rviz_plugin 2>&1 | tee build.log
+
 
 # build one package
-colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --packages-up-to autoware_ndt_scan_matcher 2>&1 | tee -a build.log
-colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --packages-select autoware_ndt_scan_matcher 2>&1 | tee -a build.log
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --parallel-workers 8 --continue-on-error --packages-up-to autoware_ndt_scan_matcher 2>&1 | tee -a build.log
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --parallel-workers 8 --continue-on-error --packages-up-to autoware_lidar_marker_localizer 2>&1 | tee -a build.log
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --parallel-workers 8 --continue-on-error --packages-up-to autoware_unified_localization 2>&1 | tee build.log
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --parallel-workers 8 --continue-on-error --packages-up-to autoware_ekf_localizer 2>&1 | tee build.log
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --parallel-workers 8 --continue-on-error --cmake-clean-cache --packages-select autoware_ndt_scan_matcher 2>&1 | tee -a build.log
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --parallel-workers 8 --continue-on-error --cmake-clean-cache --packages-select autoware_ekf_localizer 2>&1 | tee -a build.log
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --parallel-workers 8 --continue-on-error --packages-select autoware_launch 2>&1 | tee build.log
+
+
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --packages-up-to sample_vehicle_launch 2>&1 | tee -a build.log
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 8 --continue-on-error --packages-up-to aip_x2_gen2_launch 2>&1 | tee -a build.log
 
 # run test
 colcon test --packages-select autoware_ndt_scan_matcher 2>&1 | tee test.log
+colcon test --packages-select autoware_lidar_marker_localizer 2>&1 | tee test.log
+colcon test --packages-select autoware_ekf_localizer 2>&1 | tee test.log
+colcon test --packages-select autoware_ekf_localizer --event-handlers console_direct+ 2>&1 | tee test.log
+colcon test --packages-select autoware_unified_localization 2>&1 | tee test.log
+# display test results
+colcon test-result --all --verbose 2>&1 | tee -a test.log
+# display test results of a specific package
+colcon test-result --all --verbose --test-result-base build/autoware_ekf_localizer 2>&1 | tee -a test.log
+colcon test-result --all --verbose --test-result-base build/autoware_ndt_scan_matcher 2>&1 | tee -a test.log
+colcon test-result --all --verbose --test-result-base build/autoware_lidar_marker_localizer 2>&1 | tee -a test.log
+colcon test-result --all --verbose --test-result-base build/autoware_unified_localization 2>&1 | tee -a test.log
+
+# display test results for errors and failures
 colcon test-result --verbose 2>&1 | tee -a test.log
